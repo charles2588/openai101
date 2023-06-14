402ac3199c3f5dcf332eca757aa2b86d99882d44
chuck <charles2588@gmail.com>
2023-06-13 19:35:43 -0700

    formatting

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,17 @@
 import React from 'react';
 import Head from 'next/head';
 import LawyerChat from '../components/LawyerChat';
-import styles from '../components/LawyerChat.module.css';
+import styles from './index.module.css';
 
 const IndexPage = () => {
   return (
-    <div>
+    <div className={styles.mainContainer}>
       <Head>
-        <title>Lawyer Chat</title>
+        <title>AI Lawyer</title>
         <link rel="stylesheet" href="https://unpkg.com/picnic" />
       </Head>
       <div className={styles.container}>
-        <h1>Lawyer Chat</h1>
+        <h1>AI Lawyer - Powered By OpenAI</h1>
         <LawyerChat />
       </div>
     </div>
